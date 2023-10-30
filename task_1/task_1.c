6add93846bf2a472bcc74b142d8fdac8ef82ce81
LibbyCA <146595630+LibbyCA@users.noreply.github.com>
2023-10-30 18:48:02 +0000

    fix

--- task_1/task_1.c
+++ task_1/task_1.c
@@ -4,65 +4,69 @@
 
 // describe each variable
 // class named FITNESS_DATA
-typedef struct {
-	char date[11];
-	char time[6];
-	int steps;
+typedef struct
+{
+    char date[11];
+    char time[6];
+    int steps;
 } FITNESS_DATA;
 
 // Define any additional variables here
-
-
 
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
 // Ouputs: date character array; time character array; steps character array
 void tokeniseRecord(const char *input, const char *delimiter,
-                    char *date, char *time, char *steps) {
+                    char *date, char *time, char *steps)
+{
     // Create a copy of the input string as strtok modifies the string
     char *inputCopy = strdup(input);
-    
+
     // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
-    if (token != NULL) {        strcpy(date, token);
+    if (token != NULL)
+    {
+        strcpy(date, token);
     }
-    
+
     token = strtok(NULL, delimiter);
-    if (token != NULL) {
+    if (token != NULL)
+    {
         strcpy(time, token);
     }
-    
+
     token = strtok(NULL, delimiter);
-    if (token != NULL) {
+    if (token != NULL)
+    {
         strcpy(steps, token);
     }
-    
+
     // Free the duplicated string
     free(inputCopy);
-
 }
 
-
-
 // Complete the main function
-int main() {
-    char filename [] = "FitnessData_2023.csv";
-    FILE *file = fopen(filename,"r");
-    if (file == NULL) {
+int main()
+{
+    char filename[] = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
         perror("");
         return 1;
     }
 
-int buffer_size = 100;
-char line_buffer[buffer_size];
-while (fgets(line_buffer, buffer_size, file) != NULL) {
-    printf("%s", line_buffer);
-}
+    
 
+    int buffer_size = 100;
+    char line_buffer[buffer_size];
+    while (fgets(line_buffer, buffer_size, file) != NULL)
+    {
+        printf("%s", line_buffer);
+    }
 
+    fclose(file);
 
-fclose(file);
-return 0;
-
-
+    
+    return 0;
 }
